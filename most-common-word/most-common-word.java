1ec3a0a960cace54cf2b20aa928e22ca595780f6
SuyoenHan <80475419+SuyoenHan@users.noreply.github.com>
2021-07-09 20:40:06 +0900

    Time: 16 ms (49.39%), Space: 38.9 MB (90.43%) - LeetHub

--- most-common-word/most-common-word.java
+++ most-common-word/most-common-word.java
@@ -3,9 +3,12 @@ class Solution {
         
         // 1) 대소문자를 구별하지 않도록 모두 소문자로 만들기
 		paragraph= paragraph.toLowerCase();
-		  
-        // 2) 구두점과 공백을 기준으로 split
-        String[] paragraphArr= paragraph.split("[!?',;. ]");
+
+        // 2) 정규표현식을 사용하여 구두점 제거
+        paragraph= paragraph.replaceAll("[^0-9a-zA-Z]"," ");
+        
+        // 2-1) 구두점 제거된 문장을 공백을 기준으로 split
+        String[] paragraphArr= paragraph.split(" ");
 
         // 3) 문자들을 map에 담아서 카운드
         Map<String,Integer> paraMap= new HashMap<>();
@@ -25,20 +28,20 @@ class Solution {
 		for(String bannedWord:banned) {
 			sb.append(bannedWord);
 		}
-		  
+
 		String bannedWords= sb.toString();
 		String commonWord= "";
 		int frequency=0;
-		  
+
 		Set<String> paraMapKeys= paraMap.keySet();
-		  
+
 		for(String key:paraMapKeys) {
 			if(bannedWords.indexOf(key)==-1 && paraMap.get(key)>frequency) { // 금지된 단어가 아니면서 빈도수가 큰 경우
 				frequency=paraMap.get(key);
 				commonWord=key;
 			}
 		}
-		  
+
 		return commonWord;
     } // end of public String mostCommonWord(String paragraph, String[] banned) {-------
 }
