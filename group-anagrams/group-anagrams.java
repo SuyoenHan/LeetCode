95f818ae4e88431a1caf22b6e1fb6f00d69394cf
SuyoenHan <80475419+SuyoenHan@users.noreply.github.com>
2021-07-10 22:03:54 +0900

    Update group-anagrams.java
    이전코드의 runtime은 48ms , 지금코드의 runtime은 6ms

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,36 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         
-         String orderedStr="";
-	 Map<String,String> paraMap= new HashMap<>();
-		 
-	// 1) 같은 문자로 이루어 져있는지 확인=> array로 split / 정렬 후 join => 정렬된 문자열을 key값으로 사용 
-	for(int i=0;i<strs.length;i++) {
-            if("".equals(strs[i])) strs[i]="-1"; // ""의 경우 사용되지 않는 문자 "-1"로 교체     
-	    String[] strArr= strs[i].split("");
-	    Arrays.sort(strArr);
-	    orderedStr=String.join("",strArr);
-            // System.out.println(orderedStr);
-            if(paraMap.containsKey(orderedStr)) paraMap.put(orderedStr, paraMap.get(orderedStr)+" "+strs[i]);
-		   else paraMap.put(orderedStr, strs[i]);
-        }  // end of for(int i=0;i<strs.length;i++) {----------
-		 
-		
-	 // 2) 맵에 담긴 value들을 split하여 각각 list에 담기 == 같은 애니어그램끼리 list로 묶기
-	 List<List<String>> answerList= new ArrayList<>();
-	 List<String> eachList= null;
-	 Set<String> paraMapKeys= paraMap.keySet();
+        
+	// 1) 문자열을 정렬시켜서 키 값으로 사용
+	Map<String,List<String>> map= new HashMap<>();
 
-	 for(String key: paraMapKeys) {
-		eachList=new ArrayList<>(); // 초기화
-		String[] valuesArr= paraMap.get(key).split(" ");
-		for(String str: valuesArr) {
-			if("-1".equals(str)) str="";  // 1)에서 -1로 바꿔준 ""을 
-			eachList.add(str);
-		}
-		answerList.add(eachList);
-	 } // end of for(String key: paraMapKeys) {-------------------
+	for(String str:strs) {
+		char[] strArr= str.toCharArray();
+		Arrays.sort(strArr);
+		String orderedStr= String.valueOf(strArr);
 
-	 return answerList;
+		// 2) map에 존재하지 않는 경우 실행
+		map.putIfAbsent(orderedStr, new ArrayList<>());
+
+		// 3) map에서 해당 키 값의 value(List)에 추가
+		map.get(orderedStr).add(str);
+	}
+
+	// 4) 리스트 안의 리스트 형태로 리턴
+	return new ArrayList<>(map.values());
     }
 }
