da62c510b6cdf3385915384cb3d1c294df05ec8b
SuyoenHan <80475419+SuyoenHan@users.noreply.github.com>
2021-07-12 18:05:14 +0900

    Time: 22 ms (93.45%), Space: 39.1 MB (73.89%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,52 +1,71 @@
 class Solution {
-    public String longestPalindrome(String s) {
+   	 public static String longestPalindrome(String s) {
 	        
 		 // 주어진 문자열은 숫자와 영어(대소문자)로만 구성
 		 // 가장 긴 팰린드롬 부분 문자열 구하기
 		 
+		 // 중심점에서 문자 확장하기
+		 // 1) 짝수 팰린드롬을 고려하기 위해 "#"을 중간중간 넣어주기
 		 char[] sArr= s.toCharArray();
+		 char[] newArr= new char[s.length()*2+1];
 		 
-		 int length=1;
-		 String longPalindromeWord= String.valueOf(sArr[0]);
-		 boolean flag= false; // 팰린드롬 판단 (true일 경우 팰린드롬이 아니다)
-		 
-		 // 1) 팰린드롬 문자의 시작부분과 끝 부분 index 번호 구하기
-		 for(int i=0;i<s.length()-1;i++) {
-			 for(int j=i+1;j<s.length();j++) {
-                 flag= false; // 초기화
-                 
-				 if(sArr[i]==sArr[j]) { // 2) 부분문자열의 시작과 끝 index
-                     
-                     // System.out.println(i +"  "+j);
-                     
-                     // 3) 그 사이의 문자열이 팰린드롬인지 확인
-					 for(int k=i, m=0;k<=j;k++) {
-						 if(sArr[k]!=sArr[j-m]){
-                             flag=true;
-                             break;
-                         }
-                         m++;
+		 for(int i=0;i<sArr.length;i++) {
+			newArr[2*i+1]=sArr[i];  // 1 3 5 7 9
+			newArr[2*i]= '#';   
+		 } // end of  for(int i=0;i<sArr.length;i++) {------
+		 newArr[s.length()*2]= '#';
+         
+		 // 2) 각 인덱스 별로 확장 문자 개수 넣어줄 배열 생성
+		 int[] indexArr= new int[newArr.length];
+		 for(int i=0, radius=0;i<newArr.length;i++) {
+			 radius=0;
+             
+			 if(i==0) indexArr[i]=0;
+			 else {
+				 while(true) {
+					 if(i-radius-1>0 && i+radius+1<newArr.length && newArr[i-radius-1]==newArr[i+radius+1]) { // 팰린드롬인 경우
+						
+                         radius++;  
+						 continue;
 					 }
-					 
-					 if(!flag && (j-i+1) > length) { // 팰린드롬이면서 길이가 긴 경우
-                             //System.out.println("변경");
-							 length= j-i+1;
-							 longPalindromeWord= combineString(i,j, sArr); // 문자열 합치는 메서드
+					 else {
+						 break; // 확장 중지
 					 }
-					 
-				 } // end of if(sArr[i]==sArr[j]) { -----
-			 } // end of for(int j=i+1;j<s.length();j++) {-----
-		 } // end of for(int i=0;i<s.length();i++) {-----
+				 }
+				 indexArr[i]=radius;
+                 // System.out.println(i+"  "+indexArr[i]);
+			 }
+		 } // end of  for(int i=0;i<newArr.length;i++) {--------
 		 
+		 // 3) 최대 숫자가 0인 경우 첫번재 문자 반환
+		 int maxRadius=0;
+		 int centerindex=0;
+		 for(int i=0;i<indexArr.length;i++) {
+			 if(maxRadius<indexArr[i]) {
+				 maxRadius=indexArr[i];
+				 centerindex=i;
+			 }
+		 }
+
+         //System.out.println(centerindex);
+         //System.out.println(maxRadius); 
+         
+		 String palindrome="";
+		 if(maxRadius==0)  palindrome=sArr[0]+"";
+		 else {
+			 palindrome= combineString(centerindex-maxRadius,centerindex+maxRadius,newArr);
+		 }
+		 return palindrome;
 		 
-		 return longPalindromeWord;
 	 } // end of public String longestPalindrome(String s) {----
 	 
 	 // 문자열 합치는 메서드
-	 public static String combineString(int i, int j, char[] sArr) {
+	 public static String combineString(int i, int j, char[] newArr) {
 		 
 		 StringBuilder sb= new StringBuilder();
-		 for(int k=i;k<=j;k++) sb.append(sArr[k]);
+		 for(int k=i;k<=j;k++) {
+			 if(newArr[k]!='#') sb.append(newArr[k]);
+		 }
 		 
 		 return sb.toString();
 	 } // end of  public static String combineString(int i, int j, char[] sArr) {----
