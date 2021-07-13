9d2c89d6c46e2c35ef7ab367ad57ecc35d2e7e1f
SuyoenHan <80475419+SuyoenHan@users.noreply.github.com>
2021-07-13 14:13:42 +0900

    Update trapping-rain-water.java
    완전탐색으로 풀이한 경우 runtime은 338ms / 투포인터를 이용하여 풀이한 경우 runtime은 2ms

--- trapping-rain-water/trapping-rain-water.java
+++ trapping-rain-water/trapping-rain-water.java
@@ -1,49 +1,32 @@
 class Solution {
      public int trap(int[] height) {
 	     
-		 int startIndex= 0;
-		 int trapCnt=0;
+	// 투포인터 방법으로 접근 => 왼쪽 끝과 오른쪽 끝에서 최대지점으로 좁혀오기
+	int leftIndex=0;
+	int rightIndex=height.length-1;
 		 
-		 // 1) 0 이외의 숫자가 나오는 곳이 시작점
-		 for(int i=0;i<height.length;i++) {
-			 if(height[i]!=0) {
-				 startIndex=i;
-				 break;
-			 }
-		 }
+        // 빈 배열인 경우 고려
+        if(height.length==0) return 0;
+         
+	int maxLeft=height[leftIndex];
+	int maxRight=height[rightIndex];
 		 
-		 // 2) 왼쪽으로 자기 자신보다 큰 수가 있어야하고, 오른쪽으로도 자기 자신보다 큰 수가 있어야 한다
-	     for(int i=startIndex+1, position=0; i<height.length-1;i++) {
-             
-              position=0; // 초기화
-             
-	    	 // 2-1) index를 기준으로 왼쪽에서 가장 큰 값
-	    	 int maxLeft= 0;
-	    	 while(i-position>=startIndex) {
-	    		 if(height[i]<height[i-position] && maxLeft<height[i-position] ) {
-	    			 maxLeft=height[i-position];
-	    		 }
-                 position++;
-	    	 }
-	    	 
-	    	 position=0; // 초기화
-	    	 // 2-2) index를 기준으로 오른쪽에서 가장 큰 값
-	    	 int maxRight= 0;
-	    	 while(i+position<=height.length-1) {
-	    		 if(height[i]<height[i+position] && maxRight<height[i+position] ) {
-	    			 maxRight=height[i+position];
-	    		 }
-                 position++;
-	    	 }
-	    	 
-	    	 
-	    	// 3) 왼쪽과 오른쪽 숫자 중에 가장 작은 수-본인수 가 빗물이 고이는 정도
-	    	 if(maxLeft!=0 && maxRight!=0) { // 빗물이 고이는 경우
-	    		 trapCnt+=Math.min(maxLeft, maxRight)-height[i];
-	    	 }
-	    	 
-	     } // end of for--------------------
-	     
+	int trapCnt=0;
+	while(leftIndex<rightIndex) { // leftIndex와 rightIndex는 겹치면 안된다
+			 
+		if(maxLeft<=maxRight) { // 오른쪽이 더 높은 경우
+			trapCnt+=maxLeft-height[leftIndex];
+			leftIndex++;
+		}
+		else { // 왼쪽이 더 높은 경우
+			trapCnt+=maxRight-height[rightIndex];
+			rightIndex--;
+		}
+				 
+		// 최대점 갱신
+		maxLeft= Math.max(maxLeft, height[leftIndex]);
+		maxRight= Math.max(maxRight, height[rightIndex]);
+	}
 	     return trapCnt;
 	} // end of public int trap(int[] height) {----------------------
 }
